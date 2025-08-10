1235d5462a8807f0cbcfd99fbcb1685b3d73f882
needzc <needzc@126.com>
2025-08-10 09:16:13 +0800

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -6,8 +6,8 @@ const pwaOption: Partial<VitePWAOptions> = {
   includeAssets: ["icon.svg", "apple-touch-icon.png"],
   filename: "swx.js",
   manifest: {
-    name: "热点新闻",
-    short_name: "热点新闻",
+    name: "NewsNow",
+    short_name: "NewsNow",
     description: "Elegant reading of real-time and hottest news",
     theme_color: "#F14D42",
     icons: [
