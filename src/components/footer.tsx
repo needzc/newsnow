c804c10ecdf31540a65ebd5f5fecf07dbb19f45c
needzc <needzc@126.com>
2025-08-23 22:15:50 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <>
       <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
       <span>
-        <span>Allys © 2024 By </span>
+        <span>智诚今日热点 © 2024 By </span>
         <a href={Author.url} target="_blank">
           {Author.name}
         </a>
